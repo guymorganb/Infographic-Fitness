bcad7135fc30b90c3317d15e4c8e869e74301189
guymorganb <guymorganb@gmail.com>
2023-12-20 22:32:07 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# Infographic-Fitness
-An infographic developed for a technical writing course demonstrating key points of student wellness relating to fitness levels
+## Research Plan on the Impact of Physical Fitness on Student Wellness
+
+This repository contains the code and resources for the research project exploring the impact of physical fitness on student wellness. The goal is to develop a static webpage as an infographic and a video presentation of the findings.
+
+---
+
+📄 **Objective:**
+
+The proposed research aims to investigate the connections between physical fitness and various aspects of student wellness, including emotional, mental, and physical health. The project will culminate in the creation of an engaging infographic backed by scholarly research.
+
+---
+
+🔍 **Research Goals:**
+
+- Evaluate the link between fitness and student emotional health.
+- Assess the connection between fitness and student mental health.
+- Investigate the relationship between fitness and student physical health.
+- Design an infographic that concisely communicates the research findings.
+
+---
+
+🛠️ **Methodology:**
+
+Utilizing a blend of programming expertise and design principles, the research will follow these steps:
+
+1. Develop a static webpage to serve as an interactive infographic.
+2. Adhere to web accessibility standards as per course textbook recommendations.
+3. Apply design fundamentals to enhance user experience.
+4. Use OBS software for screen and audio recording to present the infographic.
+
+---
+
+💡 **Development & Presentation:**
+
+The static webpage will be developed using HTML, CSS, and JavaScript, showcasing the ability to translate data into visual storytelling. The screen recording will be edited and prepared for presentation, ensuring clarity and engagement.
+
+---
+
+🔄 **Feedback:**
+
+Feedback is crucial to the iterative development process. Please provide any comments or suggestions to improve the research and its presentation.
+
+---
+
+📚 **References:**
+
+A list of scholarly articles and their findings will be included to support the research. Key studies include works by Trosclair et al. (2011), Zhu & Cheng (2022), Oliver et al. (2019), Park et al. (2023), and Pang et al. (2022).
+
+---
+
+## Installation and Setup 🛠️
+
+N/A
+
+---
+
+## Usage 🖥️
+
+N/A
+
+---
+
+## Contributing 🤝
+
+Guidelines for contributing to the project, including coding standards, pull request process, etc.
+
+---
+
+## License 📜
+
+Information about the project license (e.g., MIT, GPL, Apache).
+
+---
+
+## Contact 📧
+
+How to reach out for more information or support.
+
+---
+
+🌟 **Acknowledgments:**
+
+Special thanks to those who have contributed to the research and development of this project.
+
+---
+
+🚀 Ready to explore the connection between fitness and wellness in college students? Let's dive in!
+
+This README.md provides a structured overview of the project, its objectives, and how 
---
+++ scripts/script.js
@@ -0,0 +1,103 @@
+const data = {
+    labels: ['Gender','Year in College', 'GPA', 'Age', 'Height', 'Weight', 'Hand Grip Strength', 'Leg Press 1RM', 'Leg Extension 1RM'],
+    datasets: [{
+        label: 'Correlation with BMI',
+        data: [0.332, -0.253, -0.404,  0.227, 0.231, 0.823, 0.414, 0.424, 0.306],
+        backgroundColor: ['green','red', 'red', 'green', 'green', 'green', 'green', 'green', 'green'],
+        
+    }]
+};
+
+// Configuration
+const config = {
+    type: 'bar',
+    data: data,
+    options: {
+        responsive: true, // Make the chart responsive
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    color: 'blue' // Change y-axis label color
+                },
+                grid: {
+                    drawBorder: false,
+                    color: function(context) {
+                        if (context.tick.value === 0) {
+                            return '#000000'; // Black color for zero line
+                        }
+                        return '#000000'; // Light grey for other grid lines
+                    },
+                    lineWidth: function(context) {
+                        if (context.tick.value === 0) {
+                            return 3; // Thicker line for zero
+                        }
+                        return 1; // Regular width for other grid lines
+                    }
+                }
+            },
+            x: {
+                ticks: {
+                    color: 'blue', // Change x-axis label color
+                    font: {
+                        size: 11 // Change the font size for x-axis labels
+                    }
+                },
+                grid: {
+                    drawBorder: false,
+                    display: false
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                labels: {
+                    color: 'purple' // Change legend text color
+                }
+            },
+            title: {
+                display: true,
+                text: 'Fitness & BMI Correlation in College Students',
+                color: 'blue', // Change title text color
+                font: {
+                    size: 18 // Font size for title
+                }
+            },
+            datalabels: {
+                color: '#000000', // Label text color
+                anchor: 'end', // Position of the label
+                align: 'top', // Alignment of the label
+                formatter: (value, context) => {
+                    return value.toFixed(2); // Format the label value
+                },
+                font: {
+                    size: 10 // Set the font size here
+                }
+            },
+        }
+    },
+    plugins: [ChartDataLabels] // Register the plugin
+};
+
+
+// Render Chart
+const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById('toggleAside');
+    var asideElement = document.querySelector('aside');
+
+    toggleButton.addEventListener('click', function () {
+        if (asideElement.style.left === '0px') {
+            asideElement.style.left = '-200px';
+        } else {
+            asideElement.style.left = '0px';
+        }
+    });
+});
+
+
